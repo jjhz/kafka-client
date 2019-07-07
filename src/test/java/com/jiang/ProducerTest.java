a2ef67d4f9bab54c41970e99c72411961dc8625d
James Jiang <james.jiang@servian.com.au>
2019-07-07 19:34:48 +1000

    add more test case

---
+++ src/test/java/com/jiang/ProducerTest.java
@@ -0,0 +1,14 @@
+package com.jiang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProducerTest {
+
+
+  @Test
+  public void testProducer(){
+    Assert.assertEquals("1","1");
+  }
+
+}
